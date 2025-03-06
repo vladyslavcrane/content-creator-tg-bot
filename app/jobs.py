020a1b36a2747c688ebfd1e3640653ed03972c8f
Vladyslav Zhuravel <vladjrvl@gmail.com>
2025-03-06 22:38:39 +0300

    feat: Add mongodb

--- app/jobs.py
+++ app/jobs.py
@@ -15,9 +15,10 @@ log = logging.getLogger(__name__)
 
 async def post_movie(bot: Bot, chat_id: int, user: Optional[User] = None) -> Dict:
     # image = URLInputFile(openai_response["poster_image_url"])
-    data = await fetch_post_movie_data()
+    moovie = await fetch_post_movie_data()
+
     album_builder = MediaGroupBuilder()
-    media_urls = parse_imdb_urls(data["imdb_link"])
+    media_urls = parse_imdb_urls(moovie.imdb_link)
 
     for media_url in media_urls:
         try:
@@ -28,24 +29,26 @@ async def post_movie(bot: Bot, chat_id: int, user: Optional[User] = None) -> Dic
             pass
 
     try:
-        data["genres"] = ", ".join(g.lower() for g in data["genres"])
-        data["director"] = (
-            data["director"]
-            if isinstance(data["director"], str)
-            else ", ".join(data["director"])
+        moovie.genres = ", ".join(g.lower() for g in moovie.genres)
+        moovie.director = (
+            moovie.director
+            if isinstance(moovie.director, str)
+            else ", ".join(moovie.director)
         )
-        data["actors"] = ", ".join(data["actors"])
+        moovie.actors = ", ".join(moovie.actors)
 
-        caption = get_rendered_template("post_random_movie.html", data)
+        caption = get_rendered_template("post_random_movie.html", {'obj': moovie})
         album_builder.caption = caption
 
-        log.info(f'Posting with data:\n{data}')
+        log.info(f'Posting with data:\n{moovie}')
 
         messages = await bot.send_media_group(chat_id=chat_id, media=album_builder.build())
         
         if user:
             await bot.forward_messages(chat_id=user.id, from_chat_id=chat_id, message_ids=[message.message_id for message in messages])
             await bot.send_message(text="Post success!", chat_id=user.id, reply_markup=admin_inline)
+            await moovie.update({'$set': {'posted': True}})
+
     except Exception as e:
         await bot.send_message(text='Post failed', chat_id=user.id)
         log.exception(f"Error posting movie: {e}")
